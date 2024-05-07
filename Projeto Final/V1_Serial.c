c3f462bf332658364f652132b28d501be6265754
Gustavo Aragão Guedes <64610385+Gustavo-Aragao-Guedes@users.noreply.github.com>
2024-05-06 22:22:00 -0300

    Create V1_Serial.c

---
+++ Projeto Final/V1_Serial.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+double calcular_e(int n) {
+    double e = 1.0;
+    double termo = 1.0;
+
+    for (int i = 1; i <= n; i++) {
+        termo /= i;
+        e += termo;
+    }
+
+    return e;
+}
+
+int main() {
+    double epsilon = 1e-10; // Critério de parada
+    double e_anterior = 0.0;
+    double e_atual = 1.0;
+    int n = 1;
+    double tempo = 0.0;
+
+    while (fabs(e_atual - e_anterior) > epsilon) {
+        e_anterior = e_atual;
+        e_atual = calcular_e(n);
+        tempo += 1.0 / e_atual;
+        n++;
+    }
+
+    printf("Valor de 'e' final: %.10f\n", e_atual);
+    printf("Tempo para atingir esse valor: %.10f segundos\n", tempo);
+
+    return 0;
+}
