42c2b1ba2e2225b47c0368721b4ef2bdc183e375
Gustavo Aragão Guedes <64610385+Gustavo-Aragao-Guedes@users.noreply.github.com>
2024-05-07 00:02:25 -0300

    Update V1_Serial.c

--- Projeto Final/V1_Serial.c
+++ Projeto Final/V1_Serial.c
@@ -14,7 +14,7 @@ double calcular_e(int n) {
 }
 
 int main() {
-    double epsilon = 1e-25; // Critério de parada
+    double epsilon = 1e-50; // Critério de parada
     double e_anterior = 0.0;
     double e_atual = 1.0;
     int n = 1;
@@ -28,7 +28,7 @@ int main() {
     }
 
     printf("Valor de 'e' final: %.25f\n", e_atual);
-    printf("Tempo para atingir esse valor: %.25f segundos\n", tempo);
+    printf("Tempo para atingir esse valor: %.50f segundos\n", tempo);
 
     return 0;
 }
